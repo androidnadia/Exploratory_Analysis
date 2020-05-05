ddcef00799005ff5a0d0901aef66135db4bc2efa
androidnadia <64418446+androidnadia@users.noreply.github.com>
2020-05-05 20:07:53 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ datetime <- strptime(date_time, "%Y-%m-%d %H:%M:%S")
 with(power, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
 points(datetime, power$Sub_metering_2, type = "l", col = "red")
 points(datetime, power$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col = c("black", "red", "blue"), lty = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # Save as PNG
 dev.copy(png, file = "plot3.png")
