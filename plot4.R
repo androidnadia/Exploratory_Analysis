ae2b1db84913460757d074f31843eb5d6efd5c70
androidnadia <64418446+androidnadia@users.noreply.github.com>
2020-05-05 20:09:02 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ with(power, plot(datetime, Voltage, type = "l"))
 with(power, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
 points(datetime, power$Sub_metering_2, type = "l", col = "red")
 points(datetime, power$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col = c("black", "red", "blue"), lty = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 with(power, plot(datetime, Global_reactive_power, type = "l"))
 
